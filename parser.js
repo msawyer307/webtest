552af42c66398212351cb87a502a4a52fbf4bc1c
msawyer307 <msawyer307@gmail.com>
2016-02-06 17:19:13 -0500

    External javascript

---
+++ parser.js
@@ -0,0 +1,7 @@
+<script>
+	function updateTestingParagraph(typedTextArea, displayParagraph) {
+		var newText = typedTextArea.value;
+		newText = newText.replace(/\r?\n/g, '<br />');
+		displayParagraph.innerHTML = newText;
+	}
+</script>
